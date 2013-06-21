f29adc793550066e58dfbf8d0ae5d5e981ff8971
Gbenga Ojo <service@lucidmediaconcepts.com>
2013-06-21 05:46:23 -0600

    cleaning up working directory

--- workspace2/config/.htusers.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	// ensure this file is being included by a parent file
-	if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
-	$GLOBALS["users"]=array(
-	array("admin","b418c90734057bd018870ec0dd3cb369","/home2/joyofcod/public_html/intro","http://localhost",1,"",7,1),
-	array("bud","b418c90734057bd018870ec0dd3cb369","/home2/joyofcod/public_html/intro/workspace2/bud","http://www.introtohtml.net",0,"",1,1),
-); 
-?>
