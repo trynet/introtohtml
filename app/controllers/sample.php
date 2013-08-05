a864eabe12ba634231ee842bd3a21926a69a9d9a
Gbenga Ojo <service@lucidmediaconcepts.com>
2013-08-05 05:23:05 -0600

    login tied into joy of code

--- app/controllers/sample.php
+++ app/controllers/sample.php
@@ -3,7 +3,7 @@
 sample.php
 Author: Gbenga Ojo <service@lucidmediaconcepts.com>
 Origin Date: July 3, 2013
-Modifed: July 3, 2013
+Modifed: August 4, 2013
 
 Process, register, and login a user after the choose to
 try the first two lessons
@@ -11,13 +11,15 @@ try the first two lessons
 
 require_once '../config/config.php';
 
-$firstname = htmlspecialchars($_GET['firstname']);
-$username  = htmlspecialchars($_GET['username']);
-$password  = md5(htmlspecialchars($_GET['password']));
+$firstname        = htmlspecialchars($_POST['firstname']);
+$username         = htmlspecialchars($_POST['username']);
+$password         = md5(htmlspecialchars($_POST['password']));
+$promotional_code = htmlspecialchars($_POST['promotional_code']);
 
 $data = array('firstname' => $firstname,
               'email'     => $username,
-              'password'  => $password);
+              'password'  => $password,
+              'promo'     => $promotional_code);
 
 $userObj = new User();
 $user_id = $userObj->addUser($data);
@@ -26,5 +28,5 @@ if ($user_id) { // registration and subsequent login OK
    // persist
    $authNamespace = new Zend_Session_Namespace('Zend_Auth');
    $authNamespace->logged_in = true;
-   header('Location: /index.php');
+   header('Location: http://dev.introtohtml.net/login.php?email=' . $username);
 }
--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<?php $email = $_GET['email'] ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +8,8 @@
 
 <body>
    <center>
-   <form name="login" action="app/controllers/login.php">
-   username: <input type="text" name="username" /><br />
+   <form name="login" action="app/controllers/login.php" method="post">
+   username: <input type="text" name="username" value="<?php echo $email ?>" /><br />
    password: <input type="password" name="password" /><br />
    <input type="submit" name="submit" value="submit" />
    </form>
