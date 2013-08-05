58c5f391f4b0330dc22581b8cf58aa3bec7b1320
Gbenga Ojo <service@lucidmediaconcepts.com>
2013-08-04 18:00:03 -0600

    new workspace uploader working properly

---
+++ app/controllers/workspace.php
@@ -0,0 +1,10 @@
+<?php
+/*-----------------------------------------------------------
+workspace controller
+Author: Gbenga Ojo <service@lucidmediaconcepts.com>
+Origin Date: August 4, 2013
+Modified: August 4, 2013
+------------------------------------------------------------*/
+require_once '../config/config.php';
+
+$action =  htmlspecialchars('action');
